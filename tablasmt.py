6ce9103e64492e6e86d1968e12b1c1df1943b26c
juanAMR <100029474+juanAMR@users.noreply.github.com>
2022-02-19 08:20:29 -0500

    Add files via upload

---
+++ tablasmt.py
@@ -0,0 +1,9 @@
+from re import X
+
+
+print("#"*50)
+print("tablas de multiplicar")
+print("#"*50)
+numero=int(input ("ingrese un numero entero" ))
+for i in range(11):
+    print(numero, "X", i, "=", numero / i )
---
+++ trabajo1.py
@@ -0,0 +1,7 @@
+#while True:
+#    print ("esty atrapado en un loop")
+i = 0
+while i < 30:
+    i=i + 1
+    print(i)
+#print("hola, 1 + 2")
